97b108b76d9bd3fd74c5c261820412769e86f408
唐道勇 <matias@tangdaoongdeMBP.lan>
2021-12-02 16:56:18 +0800

    feat: - 添加首屏加载loading

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <!--
  * @Author: your name
  * @Date: 2021-10-15 15:54:50
- * @LastEditTime: 2021-11-08 16:12:34
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-02 16:51:34
+ * @LastEditors: matiastang
  * @Description: In User Settings Edit
- * @FilePath: /vue-scaffold/src/App.vue
+ * @FilePath: /datumwealth-openalpha-front/src/App.vue
 -->
 <template>
     <!-- 路由出口 -->
@@ -35,6 +35,10 @@ export default defineComponent({
         } as App
     },
     setup() {
+        const loadingElement = document.getElementById('Loading')
+        if (loadingElement) {
+            loadingElement.remove()
+        }
         return {
             phone: '18380449615',
         }
