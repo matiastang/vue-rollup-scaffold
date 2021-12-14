e5f2d52a35fb56631f38b59a35cf59bb4943a541
唐道勇 <matias@tangdaoongdeMBP.lan>
2021-12-14 09:44:51 +0800

    fix: - 申请试用显示逻辑调整

--- src/views/web/interfaceCall/InterfaceCall.vue
+++ src/views/web/interfaceCall/InterfaceCall.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-11-10 10:19:32
- * @LastEditTime: 2021-12-08 19:09:59
+ * @LastEditTime: 2021-12-14 09:44:18
  * @LastEditors: matiastang
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /datumwealth-openalpha-front/src/views/web/interfaceCall/InterfaceCall.vue
@@ -302,7 +302,7 @@
             @okAction="applyTrialOkAction"
             @cancelAction="applyTrialCancelAction"
         />
-        <InterfaceAffix v-if="isApplyTry === 0" @click="showApplyTrialModel" />
+        <InterfaceAffix v-if="isApplyTry !== 1" @click="showApplyTrialModel" />
     </div>
 </template>
 <script lang="ts">
